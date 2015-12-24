12ad35c37035700d81598e15f5abd6c6afbd9840
georgebbbb <510223397@qq.com>
2015-12-24 17:28:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 var config = require("./webpack.config.js");
 var webpack = require("webpack")
 var webpackDevServer=require("webpack-dev-server")
-var express = require('express');
+
 config.entry.unshift("webpack-dev-server/client?http://localhost:8080", "webpack/hot/dev-server");
 var compiler = webpack(config);
 var request = require('request');
-var app = express();
 
-    app.get("/api/*",function(req, res){
-      var path = req.originalUrl;
-      request.get('http://120.55.125.236:8098'+path,
-        { form: req.query },
-        function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var obj = JSON.parse(body)
-            res.json(obj);
-          }
-        });
-    });
-    app.listen(8088);
 
 
 
